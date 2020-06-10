1e1111c55b7aa5ca7db1d9117b3dd48e47ae57f6
Rezwan Shiblu <46033464+Rezwan81@users.noreply.github.com>
2020-06-10 16:28:33 +0600

    Update rz-bks-payment-gateway.php

--- rz-bks-payment-gateway.php
+++ rz-bks-payment-gateway.php
@@ -11,19 +11,6 @@ Author URI:        http://devles.com
 License:           GPL v2 or later
 License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
 Text Domain:       janets
-
-RZ bKash Payment Gateway is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-any later version.
- 
-RZ bKash Payment Gateway is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
- 
-You should have received a copy of the GNU General Public License
-along with RZ bKash Payment Gateway. If not, see http://www.gnu.org/licenses/gpl-2.0.txt.
 */
 
 
